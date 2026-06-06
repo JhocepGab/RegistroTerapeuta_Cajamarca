c546ba0766dc3b223a69a51ddbde220b80210c6a
JeanPher <snkje123@gmail.com>
2026-06-06 12:41:57 -0500

    Add files via upload

---
+++ Biblio_Yopla_Rojas/Cita.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_Yopla_Rojas
+{
+    public class Cita
+    {
+        public int dniCliente;
+        public string nombreCliente;
+        public string anfitriona;
+        public DateTime fechaHoraInicio;
+        public int duracionMin;
+        public decimal precio;
+
+        public override string ToString()
+        {
+            return string.Format("{0:dd/MM HH:mm} - {1} con {2} ({3} min, S/. {4})",
+                fechaHoraInicio, nombreCliente, anfitriona, duracionMin, precio);
+        }
+    }
+}
---
+++ Biblio_Yopla_Rojas/ColaEspera.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_Yopla_Rojas
+{
+    public class ColaEspera
+    {
+        public NodoCola frente = null;
+        public NodoCola fin = null;
+
+        public bool EstaVacia()
+        {
+            return frente == null;
+        }
+
+        public void Encolar(Cita c)
+        {
+            NodoCola nuevo = new NodoCola();
+            nuevo.dato = c;
+
+            if (fin == null)
+            {
+                frente = nuevo;
+                fin = nuevo;
+            }
+            else
+            {
+                fin.sig = nuevo;
+                fin = nuevo;
+            }
+        }
+
+        public Cita Desencolar()
+        {
+            if (frente == null) return null;
+
+            Cita c = frente.dato;
+            frente = frente.sig;
+
+            if (frente == null)
+            {
+                fin = null;
+            }
+            return c;
+        }
+    }
+}
